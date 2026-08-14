57b4a2cf5b29cac594360b7dc7d503f4fc938c9c
felipe89mello <felipe89mello@gmail.com>
2026-08-14 09:22:19 -0300

    Ajuste js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = "/api";
 
 // ---------------------------------------------------------------
 // Configuração de cada módulo: de onde vem o dado e como exibir/editar.
